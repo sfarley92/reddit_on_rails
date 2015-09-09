66fdba745f49842e96a88915303b68b37fa7966e
Stewart Farley <SFarley92@yahoo.com>
2015-09-09 11:31:32 -0400

    votes table

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+  has_many :votes
   has_many :comments
   validates :title, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824150843) do
+ActiveRecord::Schema.define(version: 20150909145353) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "message"
@@ -49,5 +49,11 @@ ActiveRecord::Schema.define(version: 20150824150843) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "link_id"
+    t.boolean "up"
+  end
 
 end
