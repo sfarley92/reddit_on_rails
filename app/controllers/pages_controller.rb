5f2904358ee509bcab40a03b71c91f348b334dd3
Stewart Farley <SFarley92@yahoo.com>
2015-09-17 14:43:56 -0400

    adding emails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @users = User.order('created_at DESC')
     @links = Link.order('created_at DESC').page(1).per_page(100)
     # @links = Link.all.paginate(page: (page_params[:page] || 1)).order('created_at DESC')
   end
